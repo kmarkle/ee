5e2d736de20ca85de4eb6ba55c91be64401268a2
Elliot Cohen <elliot.cohen@gmail.com>
2011-07-14 17:56:35 -0400

    initial full import of rails app

---
+++ ee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ee::Application.config.secret_token = 'ae5d2dac2b91e4f1b7d2002881fed7d5205baa5bcb9165b1ad79ffa39a5d57c86bc3182dc8006ee6fad0e3e66c95f8d0d94e8bb08160316f9c8732b10b189536'
